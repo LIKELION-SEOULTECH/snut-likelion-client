b93f7cae4dd2eae948187b88f83e269397ecfaff
kyinn1307 <shruddls1307@gmail.com>
2025-05-25 17:27:49 +0900

    feat:implement Page routing and tab ui in project page(#24)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "@/pages/Home";
 import { ROUTES } from "./constants/routes";
+import ProjectPage from "./pages/Project";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path={ROUTES.HOME} element={<Home />} />
+                <Route path={ROUTES.PROJECT} element={<ProjectPage />} />
             </Routes>
         </BrowserRouter>
     );
---
+++ src/pages/Project.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import CategoryTabs from "@/components/project/CategoryTabs";
+import GenerationTabs from "@/components/project/GenerationTabs";
+import PageLayout from "@/layouts/PageLayout";
+import ProjectList from "@/components/project/ProjectList";
+import { mockProjectList } from "@/constants/mockProjectData";
+import QuoteCardList from "@/components/project/QuoteCardList";
+
+export default function ProjectPage() {
+    const [generation, setGeneration] = useState("전체");
+    const [category, setCategory] = useState("전체");
+
+    const filteredProjects = mockProjectList.filter((project) => {
+        const matchGen = generation === "전체" || project.class === generation;
+        const matchCat = category === "전체" || project.tag === category;
+        return matchGen && matchCat;
+    });
+
+    return (
+        <PageLayout>
+            <div className="w-full flex flex-col text-white bg-[#111111] items-center px-28">
+                <div className="font-extrabold text-7xl mt-[85px] mb-18">
+                    Project Archive<span className="text-[#FF7700]">.</span>
+                </div>
+                <GenerationTabs selected={generation} onSelect={setGeneration} />
+                <CategoryTabs selected={category} onSelect={setCategory} />
+                <ProjectList projects={filteredProjects} />
+                <QuoteCardList />
+            </div>
+        </PageLayout>
+    );
+}
