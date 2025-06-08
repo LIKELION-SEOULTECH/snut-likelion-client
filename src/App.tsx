25237a5dbdabc5ef1c8c1528239d62d568722738
kyinn1307 <shruddls1307@gmail.com>
2025-06-09 03:36:03 +0900

    feat: add blog page route(#30)

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "@/pages/Home";
 import { ROUTES } from "./constants/routes";
+import { BlogPage } from "./pages/Blog";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path={ROUTES.HOME} element={<Home />} />
+                <Route path={ROUTES.BLOG} element={<BlogPage />} />
             </Routes>
         </BrowserRouter>
     );
