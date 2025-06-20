3cfe5b2ae7f52d6239777aa5eb352d03d76482e1
kyinn1307 <shruddls1307@gmail.com>
2025-06-20 15:05:27 +0900

    chore: add access token in API request (#47)

--- src/apis/axiosInstace.ts
+++ src/apis/axiosInstace.ts
@@ -1,8 +1,16 @@
 import axios from "axios";
+import { getAccessToken } from "@/utils/token";
 
 const axiosInstance = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL,
     withCredentials: true
 });
 
-export default axiosInstance;
+// accessToken 헤더에 자동 주입
+axiosInstance.interceptors.request.use((config) => {
+    const token = getAccessToken();
+    if (token && config.headers) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+});
