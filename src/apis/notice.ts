eb139c0bc8fee634269476c1937dfd169677b0d8
kyinn1307 <shruddls1307@gmail.com>
2025-06-23 01:57:32 +0900

    feat: connect notice list API and add related hooks(#51)

---
+++ src/apis/notice.ts
@@ -0,0 +1,21 @@
+import axiosInstance from "./axiosInstace";
+
+export const createNotice = (data: { title: string; content: string; pinned: boolean }) => {
+    return axiosInstance.post("/notices", data);
+};
+
+export const getAllNotices = () => {
+    return axiosInstance.get("/notices");
+};
+
+export const getNoticeById = (noticeId: number) => {
+    return axiosInstance.get(`/notices/${noticeId}`);
+};
+
+export const updateNotice = (noticeId: number, data: { title?: string; content?: string }) => {
+    return axiosInstance.patch(`/notices/${noticeId}`, data);
+};
+
+export const deleteNotice = (noticeId: number) => {
+    return axiosInstance.delete(`/notices/${noticeId}`);
+};
