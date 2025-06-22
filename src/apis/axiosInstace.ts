cb7efe69a6a6155ab0aaeec77e630feca0f28f9b
kyinn1307 <shruddls1307@gmail.com>
2025-06-23 01:55:31 +0900

    feat: add access token in axios header

--- src/apis/axiosInstace.ts
+++ src/apis/axiosInstace.ts
@@ -1,10 +1,27 @@
 import axios from "axios";
 import { getAccessToken } from "@/utils/token";
 
+// axios 인스턴스 생성
 const axiosInstance = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL,
     withCredentials: true
 });
+
+// 요청 인터셉터 추가
+axiosInstance.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("accessToken");
+
+        if (token) {
+            config.headers.Authorization = `Bearer ${token}`;
+        }
+
+        return config;
+    },
+    (error) => {
+        return Promise.reject(error);
+    }
+);
 
 // accessToken 헤더에 자동 주입
 axiosInstance.interceptors.request.use((config) => {
