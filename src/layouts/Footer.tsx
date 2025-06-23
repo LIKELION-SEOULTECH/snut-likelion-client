bbd3b33f95e16c94734688dd31d9e7671efb0f91
kyinn1307 <shruddls1307@gmail.com>
2025-06-23 16:17:22 +0900

    feat: implement Home component in mobile devices(#52)

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -5,19 +5,24 @@ interface FooterProps {
 export const Footer = ({ white = false }: FooterProps) => {
     return (
         <div
-            className={`h-[97px] px-[111px] justify-between flex items-center text-[#666666] text-[14px] ${
+            className={`h-18 sm:h-[97px] px-4 sm:px-[111px] justify-between flex items-center text-[#666666] text-xs sm:text-[14px] ${
                 white ? "bg-white" : "bg-[#1B1B1B]"
             }`}
         >
-            <span>
+            <span className="hidden sm:inline">
                 Copyright © 2025 멋쟁이사자처럼 서울과학기술대학교 Inc. All rights reserved.
             </span>
+            <span className="sm:hidden">
+                Copyright © 2025 멋쟁이사자처럼
+                <br /> 서울과학기술대학교 Inc. All rights reserved.
+            </span>
+
             <a
                 href={"https://www.instagram.com/likelion_st/"}
                 target="_blank"
                 rel="noopener noreferrer"
             >
-                <span>Instagram</span>
+                <span className="pr-[7px] sm:pr-0">Instagram</span>
             </a>
         </div>
     );
