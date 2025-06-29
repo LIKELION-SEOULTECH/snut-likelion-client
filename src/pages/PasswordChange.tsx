0b8d2de1e07b68aef981dffcb84efd8fe7a10d29
조아 <parkjoah@naver.com>
2025-06-30 07:28:46 +0900

    feat: password change UI (#54)

--- src/pages/PasswordChange.tsx
+++ src/pages/PasswordChange.tsx
@@ -1,9 +1,17 @@
+import { AuthLayout } from "@/components/auth/AuthLayout";
+import { PasswordChangeForm } from "@/components/auth/PasswordChangeForm";
+import QuoteCardList from "@/components/project/QuoteCardList";
 import PageLayout from "@/layouts/PageLayout";
 
 export const PasswordChange = () => {
     return (
         <PageLayout>
-            <div></div>
+            <AuthLayout title="비밀번호 변경">
+                <PasswordChangeForm />
+            </AuthLayout>
+            <div className="w-full h-[150px] px-28 bg-[#1B1B1B]">
+                <QuoteCardList />
+            </div>
         </PageLayout>
     );
 };
