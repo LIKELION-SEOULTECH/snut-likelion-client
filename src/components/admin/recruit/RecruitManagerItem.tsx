1ef08774fa24f14d7e529ed13c55b9d54d00d228
kyinn1307 <shruddls1307@gmail.com>
2025-07-03 07:24:17 +0900

    change recruit file name

--- src/components/admin/recruit/RecruitManagerItem.tsx
+++ src/components/admin/recruit/RecruitManagerItem.tsx
@@ -7,7 +7,7 @@ import {
 } from "@/components/ui/select";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
-import type { ManagerData } from "@/types/recruit";
+import type { ManagerData } from "@/types/recruitment";
 import { cn } from "@/libs/cn";
 
 interface RecruitManagerItemProps {
--- src/types/recruit.ts
+++ src/types/recruit.ts
@@ -38,33 +38,3 @@ export const typeMap = {
     아기사자: "MEMBER",
     운영진: "MANAGER"
 } as const;
-
-export interface ManagerData {
-    id: number;
-    name: string;
-    email: string;
-    department: string;
-    part: string;
-    applyDate: string;
-    result: "합격" | "불합격";
-}
-
-export interface UserData {
-    id: number;
-    name: string;
-    email: string;
-    part: string;
-    applyDate: string;
-    result: "합격" | "불합격";
-}
-
-export interface UpdateQuestionRequest {
-    id?: number; // 기존 질문이면 존재
-    text: string;
-    questionType: string;
-    questionTarget: string;
-    order: number;
-    part?: string; // questionTarget === "PART"일 때만 사용
-    departmentType?: string; // questionTarget === "DEPARTMENT"일 때만 사용
-    buttonList?: string[]; // questionType === "RADIO_BUTTON"일 때만 사용
-}
