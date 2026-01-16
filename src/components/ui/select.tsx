fc0998a9e31fc7c4f782af5ffd61f30ef55a7605
Kyeongin <144935033+kyinn1307@users.noreply.github.com>
2026-01-16 16:52:43 +0900

    refactor: customize select item for drop down menu (#101)

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -68,7 +68,7 @@ function SelectContent({
                 <SelectScrollUpButton />
                 <SelectPrimitive.Viewport
                     className={cn(
-                        "p-1",
+                        "p-0",
                         position === "popper" &&
                             "h-[var(--radix-select-trigger-height)] w-full min-w-[var(--radix-select-trigger-width)] scroll-my-1"
                     )}
--- src/constants/admin/dummyMemberData.ts
+++
@@ -1,7 +0,0 @@
-import type { Member } from "@/types/members";
-export const dummyMemberData: Member[] = [
-    { id: 1, username: "박진아", generation: "13기", part: "기획", role: "대표" },
-    { id: 2, username: "박진아", generation: "13기", part: "디자인", role: "운영진" },
-    { id: 3, username: "박진아", generation: "13기", part: "AI", role: "아기사자" },
-    { id: 4, username: "김수현", generation: "12기", part: "프론트엔드", role: "아기사자" }
-];
