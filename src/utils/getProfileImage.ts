d0bc77bd49d5f1d2369813adb267336dc6421abc
Kyeongin <144935033+kyinn1307@users.noreply.github.com>
2026-02-22 05:13:52 +0900

    fix: change folder name

--- src/utils/getProfileImage.ts
+++ src/utils/getProfileImage.ts
@@ -1,5 +1,5 @@
 import { memberImageMap } from "./memberImage";
-import samplePRF from "@/assets/member/samplePRFIMG.png";
+import samplePRF from "@/assets/Member/samplePRFIMG.png";
 
 export const getProfileImage = (id?: number, profileImageUrl?: string | null) => {
     if (profileImageUrl) return profileImageUrl;
