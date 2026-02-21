221e32fa01b83da3d09e8d793add237eb45a6f88
Kyeongin <144935033+kyinn1307@users.noreply.github.com>
2026-02-22 04:19:10 +0900

    fix: fix deploy err

--- src/assets/Member/memberImage.ts
+++ src/assets/Member/memberImage.ts
@@ -5,11 +5,8 @@ const modules = import.meta.glob("/src/assets/member/profile/*.png", {
 
 export const memberImageMap: Record<string, string> = Object.fromEntries(
     Object.entries(modules).map(([path, src]) => {
-        const file = path.split("/").pop()!; // "6.png"
+        const file = path.split("/").pop()!;
         const id = file.replace(".png", "");
         return [id, src];
     })
 );
-
-// 디버깅용
-console.log("profile image count:", Object.keys(memberImageMap).length);
